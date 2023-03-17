8e6da419fa5201c9c0b04f31708a5170a778118c
Dana Chapman <dana.leigh.dlc@gmail.com>
2023-03-17 12:16:38 -0500

    Comment for user-friendly save message

--- src/Components/TextCard/TextCard.js
+++ src/Components/TextCard/TextCard.js
@@ -1,11 +1,12 @@
-export const TextCard = ({ id, text, saveStatus, addSave, deleteSave }) => {
-    const saveText = saveStatus ? 'Unsave' : 'Save'
+export const TextCard = ({ id, text, saveStatus, addSave }) => {
+    // const saveText = saveStatus ? 'Unsave' : 'Save'
 
     return (
-        <article>
+        <article className='flex flex-col'>
             <p className='text-blue-400'>
                 { text }
             </p>
+            {/* { saveStatus && <span className='text-blue-600 text-sm italic'>Saved for a rainy day.</span> } */}
             <button 
                 className='text-blue-400 border-blue-300 border-2 rounded'
                 onClick={() => addSave(id, text)}
